f5529909b9df04c5ce31032cc436a8258e2c31ec
christian albuquerque - 1717169 <1717169@7setlabs.local>
2019-08-09 18:12:09 -0300

    Aplicando rotas na aplicação

--- src/Topo.js
+++ src/Topo.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Topo = () => {
     return(
         <nav className="indigo darken-4" role="navigation">
         <div className="nav-wrapper container">
           <a id="logo-container" href="#" className="brand-logo">React Taskboard</a>
+          <Link className="right" to="/">Estórias</Link>
         </div>
       </nav>
     );
